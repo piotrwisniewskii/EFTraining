a366b34bad820527d413b512bb94599c95b9e395
Piotr Wisniewski <piowisni@gmail.com>
2023-02-10 18:20:00 +0100

    Add project files.

---
+++ MyBoards/Entities/MyBoardsContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.Metrics;
+using System;
+
+namespace MyBoards.Entities
+{
+    public class MyBoardsContext : DbContext
+    {
+        public MyBoardsContext(DbContextOptions<MyBoardsContext> options) : base(options) 
+        {
+
+        }        
+        public DbSet<WorkItem> WorkItems { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Adress> Adresses { get; set; }
+ 
+    }
+}
---
+++ MyBoards/Entities/WorkItem.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyBoards.Entities
+{
+    public class WorkItem
+    {
+        [Key]
+        public int Id { get; set; }
+        public int State { get; set; }
+        public string Area { get; set; }
+        public string IterationPath { get; set; }
+        public int Priority { get; set; }
+
+        // Epic
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+
+        //Issue
+        public decimal Efford { get; set; }
+        //Task
+        public string Activity { get; set; }
+        public decimal RemainingWork { get; set; }
+
+        public string Type { get; set; }
+    }
+}
