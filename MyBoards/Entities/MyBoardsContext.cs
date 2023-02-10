f3f6b49b8296e3ee7896828eccebd81d1fc95a9d
Piotr Wisniewski <piowisni@gmail.com>
2023-02-10 19:25:46 +0100

    1 to 1 relation between User and Adress created

--- MyBoards/Entities/MyBoardsContext.cs
+++ MyBoards/Entities/MyBoardsContext.cs
@@ -7,10 +7,10 @@ namespace MyBoards.Entities
 {
     public class MyBoardsContext : DbContext
     {
-        public MyBoardsContext(DbContextOptions<MyBoardsContext> options) : base(options) 
+        public MyBoardsContext(DbContextOptions<MyBoardsContext> options) : base(options)
         {
 
-        }        
+        }
         public DbSet<WorkItem> WorkItems { get; set; }
         public DbSet<User> Users { get; set; }
         public DbSet<Tag> Tags { get; set; }
@@ -19,25 +19,23 @@ namespace MyBoards.Entities
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<WorkItem>()
-                   .Property(x => x.State)
-                   .IsRequired();
-
-            modelBuilder.Entity<WorkItem>()
-                .Property(x => x.Area)
-                .HasColumnType("varchar(200)");
-
             modelBuilder.Entity<WorkItem>(eb =>
             {
+                eb.Property(wi => wi.State).IsRequired();
+                eb.Property(wi => wi.Area).HasColumnType("varchar(200)");
                 eb.Property(wi => wi.IterationPath).HasColumnName("Iteration_Path");
                 eb.Property(wi => wi.Efford).HasColumnType("decimal(5,2)");
                 eb.Property(wi => wi.EndDate).HasPrecision(3);
                 eb.Property(wi => wi.Activity).HasMaxLength(200);
                 eb.Property(wi => wi.RemainingWork).HasPrecision(14, 2);
+                eb.Property(wi=>wi.Priority).HasDefaultValue(1);
             });
 
-
+            modelBuilder.Entity<Comment>(eb =>
+            {
+                eb.Property(x => x.CreatedDate).HasDefaultValueSql("getutcdate()");
+                eb.Property(x => x.UpdatedDate).ValueGeneratedOnUpdate();
+            });
         }
- 
     }
 }
