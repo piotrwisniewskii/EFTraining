a85a3d82dc65191a0d98c319fa1c532dd08ec2e1
Piotr Wisniewski <piowisni@gmail.com>
2023-02-16 21:02:31 +0100

    DB filled with autogenerated data

--- MyBoards/Entities/MyBoardsContext.cs
+++ MyBoards/Entities/MyBoardsContext.cs
@@ -92,8 +92,18 @@ namespace MyBoards.Entities
                 .WithOne(a => a.User)
                 .HasForeignKey<Adress>(a => a.UserId);
 
-            
-    
+            modelBuilder.Entity<WorkItemState>()
+                .HasData
+                (new WorkItemState() {Id = 1, Value = "To Do" },
+                new WorkItemState() {Id = 2, Value = "Doing" },
+                new WorkItemState() {Id = 3, Value = "Done" });
+
+            modelBuilder.Entity<Tag>()
+                .HasData(new Tag() { Id = 1, Value = "Web" },
+                new Tag() { Id = 2, Value = "UI" },
+                new Tag() { Id = 3, Value = "Desktop" },
+                new Tag() { Id = 4, Value = "API" },
+                new Tag() { Id = 5, Value = "Service" });
         }
     }
 }
--- MyBoards/Program.cs
+++ MyBoards/Program.cs
@@ -29,5 +29,36 @@ if(pendingMigrations.Any())
     dbContext.Database.Migrate();
 }
 
+var users = dbContext.Users.ToList();
+if(!users.Any())
+{
+    var user1 = new User()
+    {
+        Email = "user1@test.com",
+        FullName = "User One",
+        Adress = new Adress()
+        {
+            City = "Warszwa",
+            Street = "Szeroka"
+        }
+    };
+
+    var user2 = new User()
+    {
+        Email = "user2@test.com",
+        FullName = "User Two",
+        Adress = new Adress()
+        {
+            City = "Krakow",
+            Street = "D³uga"
+
+        }
+    };
+
+    dbContext.Users.AddRange(user1, user2);
+
+    dbContext.SaveChanges();
+}
+
 app.Run();
 
